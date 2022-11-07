7bb90f71804f9bc0bb2104182eeffe4d7058f1b2
Nguyen Phuoc Tai <51445830+phuoctain002@users.noreply.github.com>
2022-11-07 20:23:33 +0700

    Update url.js

--- src/api/url.js
+++ src/api/url.js
@@ -1,2 +1,2 @@
-// export const url = 'https://nativecity.herokuapp.com';
-export const url = 'http://127.0.0.1:3001';
+export const url = 'https://kimjewelry.herokuapp.com';
+// export const url = 'http://127.0.0.1:3001';
